07e4c34fc18aea72114a2053b025339d68a45f7d
Duho Wise <Duhowise@gmail.com>
2019-08-25 18:59:00 +0000

    Initial Commit

---
+++ TestYates/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TestYates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            int num = 12;
+            //Stack<int> AssignedNumbers=new Stack<int>();
+            //int[] number;
+            //number=new int[10];
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    number.SetValue(i+1, i);
+            //}
+            //Shuffle.Doshuffle(number);
+            //foreach (int numbers in number)
+            //{
+            //    Console.WriteLine(numbers); 
+            //}
+
+            List<int> QuestionNumber = new List<int>();
+            for (var i = 1; i < num + 1; i++)
+            {
+                QuestionNumber.Add(i);
+                
+            }
+            foreach (var number in QuestionNumber)
+            {
+                Console.WriteLine(number);
+            }
+           Shuffle.shuffle(QuestionNumber);
+            
+
+            Stack<int> shufled = new Stack<int>();
+            //new Shuffle(QuestionNumber);
+            //foreach (int numbers in QuestionNumber)
+            //{
+            //    AssignedNumbers.Push(numbers);
+            //}
+            Shuffle.Assign(QuestionNumber,shufled);
+            for (var i = 0; i < QuestionNumber.Count; i++)
+            {
+                Console.WriteLine(QuestionNumber[i]);
+            }
+            Console.ReadLine();
+
+            #endregion
+
+
+
+        }
+    }
+}
+
+
+
---
+++ TestYates/Shuffle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestYates
+{
+    public static  class Shuffle
+    {
+        private static Random rand =new Random();
+
+        public static void shuffle(List<int> number)
+        {
+            for (var i =number.Count-1;i>0;i--)
+            {
+                int j = rand.Next(i+1);
+                int temp = number[i];
+                number[i] = number[j];
+                number[j] = temp;
+            }
+        }
+
+        public static void Assign(List<int> value, Stack<int> Values)
+        {
+            for (var i = 0; i < value.Count; i++)
+            {
+               Values.Push(value[i]);
+            }
+          
+
+        }
+    }
+}
